69a2664265e6743a3416421f07a5888e9e5dcf58
Dmytro Holiaka <holdim.pro@gmail.com>
2025-03-24 17:51:20 +0200

    General: Refactor benchmark for scripting purposes

--- src/MatrixCompute.Runner/Utils/Benchmark.cs
+++ src/MatrixCompute.Runner/Utils/Benchmark.cs
@@ -10,10 +10,6 @@ internal sealed class Benchmark
 
     internal void Run(IMultiplier multiplier, int dimension)
     {
-        Console.WriteLine($"-------------- {multiplier.GetType().Name} --------------");
-        Console.WriteLine("{0,6} | {1,6} | {2,20} ", "n", "s", "Execution time (ms)");
-        Console.WriteLine("--------------------------------------------");
-
         Matrix matrixA = Matrix.GenerateRandomMatrix(dimension, dimension);
         Matrix matrixB = Matrix.GenerateRandomMatrix(dimension, dimension);
 
@@ -22,8 +18,6 @@ internal sealed class Benchmark
         _sw.Stop();
         double duration = _sw.Elapsed.TotalMilliseconds;
 
-        Console.WriteLine("{0,6} | {1,6} | {2,20:F4}", dimension, dimension, duration);
-
-        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine(duration);
     }
 }
